ea246d2375fcc7e9d0edc7296fdbd80c465125c8
carlopisacane <doncarlopisacane@gmail.com>
2020-02-19 19:34:27 +0100

    Projects route & controller

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,4 +1,21 @@
 import { Router } from 'express'
 const router = Router()
 
+import {
+	createProject,
+	getProjects,
+	getOneProject,
+	deleteProject,
+	updateProject
+} from '../controllers/projectController'
+
+// /api/projects/
+router.post('/', createProject)
+router.get('/', getProjects)
+
+// /api/projects/:projectID
+router.get('/:id', getOneProject)
+router.delete('/:id', deleteProject)
+router.put('/:id', updateProject)
+
 export default router
